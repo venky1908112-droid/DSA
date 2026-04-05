d89997bdcfe58e68df325075eff381c1cc7560c0
Venkat <venky1908112@gmail.com>
2026-04-05 11:13:35 +0530

    Time: 1622 ms (16.94%) | Memory: 105.6 MB (23.06%) - LeetSync

--- 4268-integers-with-multiple-sum-of-two-cubes/integers-with-multiple-sum-of-two-cubes.py
+++ 4268-integers-with-multiple-sum-of-two-cubes/integers-with-multiple-sum-of-two-cubes.py
@@ -1,21 +1,19 @@
 from collections import defaultdict
-
 class Solution(object):
     def findGoodIntegers(self, n):
-        cube_count = defaultdict(int)
-        
+        cube_pair = defaultdict(int)
+
+        res = []
+
         a = 1
-        while a**3 <= n:
+        while a ** 3 <= n:
             b = a
-            while a**3 + b**3 <= n:
-                x = a**3 + b**3
-                cube_count[x] += 1
+            while a ** 3 + b ** 3 <= n:
+                x = a ** 3 + b ** 3
+                cube_pair[x] += 1
                 b += 1
             a += 1
-        
-        ans = []
-        for num in cube_count:
-            if cube_count[num] >= 2:
-                ans.append(num)
-        
-        return sorted(ans)
+        for key,val in cube_pair.items():
+            if val >= 2:
+                res.append(key)
+        return sorted(res)
