d9f6cbb8994fd97410ab8a8aa453cca3bbcce379
Venkat <venky1908112@gmail.com>
2026-04-05 06:55:12 +0530

    Time: 14 ms (63.34%) | Memory: 19.2 MB (92.29%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,13 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x = y = 0
+        for m in moves:
+            if m == 'U':
+                x += 1
+            elif m == 'D':
+                x -= 1
+            elif m == 'L':
+                y -= 1
+            else:
+                y += 1
+        return x == y == 0
