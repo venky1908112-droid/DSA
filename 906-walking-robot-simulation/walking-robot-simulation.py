5fbdd843249bf48d2754b028fbe0977b7afea020
Venkat <venky1908112@gmail.com>
2026-04-06 20:02:18 +0530

    Time: 38 ms (100.00%) | Memory: 17.2 MB (47.37%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.py
+++ 906-walking-robot-simulation/walking-robot-simulation.py
@@ -1,31 +1,33 @@
 class Solution(object):
     def robotSim(self, commands, obstacles):
-        obs = set(map(tuple, obstacles))
+        obs = set(map(tuple,obstacles))
         directions = {
-            'North'  : {-1 : 'East',-2 : 'West'},
-            'South'  : {-1 : 'West',-2 : 'East'},
-            'East'   : {-1 : 'South',-2 : 'North'},
-            'West'   : {-1 : 'North',-2 : 'South'}
+            'n'  : {-1 : 'e', -2 : 'w'},
+            's'  : {-1 : 'w', -2 : 'e'},
+            'e'   : {-1 : 's', -2 : 'n'},
+            'w'   : {-1 : 'n', -2 : 's'}
         }
         ans = 0
-        x, y = 0, 0
-        curr_d = "North"
+        x,y = 0,0
+        curr_d = 'n'
         for command in commands:
             if command < 0:
                 curr_d = directions[curr_d][command]
             else:
                 for _ in range(command):
                     nx, ny = x, y
-                    if curr_d == 'North':
+                    if curr_d == 'n':
                         ny += 1
-                    elif curr_d == 'South':
+                    elif curr_d == 's':
                         ny -= 1
-                    elif curr_d == "East":
+                    elif curr_d == 'e':
                         nx += 1
                     else:
                         nx -= 1
-                    if (nx,ny) in obs:
-                        break 
+                    if (nx, ny) in obs:
+                        break
                     x, y = nx, ny
-                ans = max(ans, x*x + y*y)
+                ans = max(ans, x*x + y *y)
         return ans
+
+        
