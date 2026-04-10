e5e0cabbab9f66114b01f33875e4f419033b2515
Venkat <venky1908112@gmail.com>
2026-04-10 17:24:36 +0530

    Time: 606 ms (5.38%) | Memory: 108.9 MB (5.38%) - LeetSync

--- 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
@@ -1,11 +1,13 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 class Solution:
     def minimumDistance(self, nums: List[int]) -> int:
-        indx = defaultdict(list)
+        indx = defaultdict(deque)
         mn = float('inf')
         for i, val in enumerate(nums):
             indx[val].append(i)
-            if len(indx[val]) >= 3:
-                f, _, l = indx[val][-3:]
+            if len(indx[val]) > 3:
+                indx[val].popleft()
+            if len(indx[val]) == 3:
+                f, _, l = indx[val]
                 mn = min(mn, (l - f) * 2)
         return -1 if mn == float('inf') else mn
