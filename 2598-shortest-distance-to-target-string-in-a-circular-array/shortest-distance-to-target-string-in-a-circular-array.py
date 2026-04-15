a1653a410b36a85bf6b9bdf13b6e1a944ed152d8
Venkat <venky1908112@gmail.com>
2026-04-15 08:35:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (31.58%) - LeetSync

--- 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.py
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.py
@@ -1,13 +1,9 @@
 class Solution(object):
     def closestTarget(self, words, target, startIndex):
         n = len(words)
-        ans = float('inf')
-        
+        mn = float('inf')
         for i in range(n):
             if words[i] == target:
-                dist = abs(i - startIndex)
-                ans = min(ans, min(dist, n - dist))
-        
-        return ans if ans != float('inf') else -1
-                
-        return min(right,left)
+                d = abs(i - startIndex)
+                mn = min(mn, min(d,n - d))
+        return mn if mn != float('inf') else -1
