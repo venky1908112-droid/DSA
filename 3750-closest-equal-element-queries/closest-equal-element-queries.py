ef3fc24f1ec6d22721861d107e7432477aa103f9
Venkat <venky1908112@gmail.com>
2026-04-16 21:05:43 +0530

    Time: 279 ms (85.16%) | Memory: 53.5 MB (92.19%) - LeetSync

--- 3750-closest-equal-element-queries/closest-equal-element-queries.py
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.py
@@ -1,35 +1,32 @@
 from collections import defaultdict
+from bisect import bisect_left
 from typing import List
 
 class Solution:
     def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         d = defaultdict(list)
-        n = len(nums)
-        
-        # Step 1: store indices of each value
         for i, x in enumerate(nums):
             d[x].append(i)
-        
-        # Step 2: precompute answers for every index
-        ans = [-1] * n
-        
-        for val in d:
-            lst = d[val]
-            k = len(lst)
-            
-            if k == 1:
-                continue
-            
-            for i in range(k):
-                cur = lst[i]
-                
-                prev = lst[i - 1] if i > 0 else lst[-1]
-                next_ = lst[i + 1] if i < k - 1 else lst[0]
-                
-                d1 = abs(cur - prev)
-                d2 = abs(next_ - cur)
-                
-                ans[cur] = min(d1, n - d1, d2, n - d2)
-        
-        # Step 3: answer queries in O(1)
-        return [ans[q] for q in queries]
+
+        res = [-1] * len(queries)
+        N = len(nums)
+
+        for i, query in enumerate(queries):
+            lst = d[nums[query]]
+            n = len(lst)
+
+            if n > 1:
+                mid = bisect_left(lst, query)
+
+                prev_idx = lst[(mid - 1 + n) % n]
+                next_idx = lst[(mid + 1) % n]
+
+                dist1 = abs(query - prev_idx)
+                dist2 = abs(query - next_idx)
+
+                dist1 = min(dist1, N - dist1)
+                dist2 = min(dist2, N - dist2)
+
+                res[i] = min(dist1, dist2)
+
+        return res
