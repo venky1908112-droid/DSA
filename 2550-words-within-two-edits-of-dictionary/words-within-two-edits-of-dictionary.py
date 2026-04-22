e1d5201c45f14fef59bf7ed900bfce97ada5be60
Venkat <venky1908112@gmail.com>
2026-04-22 20:29:10 +0530

    Time: 132 ms (11.27%) | Memory: 19.3 MB (78.43%) - LeetSync

--- 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.py
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.py
@@ -1,17 +1,9 @@
 class Solution:
     def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
-        def difference(s1, s2):
-            d = 0
-            for i in range(len(s1)):
-                if s1[i] != s2[i]:
-                    d += 1
-                if d > 2:
-                    return False
-            return True
         res = []
         for query in queries:
             for d in dictionary:
-                if difference(query, d):
+                if sum(1 for a,b in zip(query, d) if a != b) <= 2:
                     res.append(query)
                     break
         return res
