b4c27c5b7deab7c8fc12f09b6323bf8582cd9111
Venkat <venky1908112@gmail.com>
2026-04-26 11:34:09 +0530

    Time: 288 ms (84.74%) | Memory: 53.7 MB (92.83%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.py
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.py
@@ -4,8 +4,9 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
         visited = [[False] * n for _ in range(m)]
-        def bfs(i , j):
-            directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
+        def cycle(i, j):
+            directions = [(0,1), (1,0), (0,-1), (-1,0)]
             q = deque()
             q.append((i, j, -1, -1))
             visited[i][j] = True
@@ -14,22 +15,25 @@ class Solution:
                 for dx, dy in directions:
                     nx = x + dx
                     ny = y + dy
+
                     if nx < 0 or ny < 0 or nx >= m or ny >= n:
                         continue
-                    if grid[x][y] != grid[nx][ny]:
+                    
+                    if grid[nx][ny] != grid[x][y]:
                         continue
-                    if px == nx and py == ny:
+                    
+                    if nx == px and ny == py:
                         continue
                     if visited[nx][ny]:
                         return True
                     visited[nx][ny] = True
                     q.append((nx, ny, x, y))
             return False
-                    
+
 
         for i in range(m):
             for j in range(n):
                 if not visited[i][j]:
-                    if bfs(i,j):
+                    if cycle(i , j):
                         return True
         return False
