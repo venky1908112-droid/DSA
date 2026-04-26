f109adfed424b5b2ab32e79587c732567387baf2
Venkat <venky1908112@gmail.com>
2026-04-26 12:41:19 +0530

    Time: 69 ms (73.96%) | Memory: 32.8 MB (13.57%) - LeetSync

--- 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.py
+++ 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.py
@@ -1,9 +1,8 @@
 class Solution:
     def minOperations(self, nums):
-        cost = 0
-        
-        for i in range(1, len(nums)):
-            if nums[i - 1] > nums[i]:
-                cost += nums[i - 1] - nums[i]
-        
-        return cost
+        n = len(nums)
+        ans = 0
+        for i in range(1, n):
+            if nums[i-1] > nums[i]:
+                ans += nums[i-1] - nums[i]
+        return ans
