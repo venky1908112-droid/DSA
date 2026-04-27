ad2ac886f086672efc8b49d58e8195fad982f86d
Venkat <venky1908112@gmail.com>
2026-04-27 20:55:15 +0530

    Time: 181 ms (73.51%) | Memory: 52.8 MB (32.46%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
@@ -3,7 +3,7 @@ class Solution:
         self.m = 0
         self.n = 0
         self.directions = {
-            1: [(0, -1), (0, 1)],
+            1: [(0,-1 ), (0, 1)],
             2: [(-1, 0), (1, 0)],
             3: [(0, -1), (1, 0)],
             4: [(0, 1), (1, 0)],
@@ -11,22 +11,25 @@ class Solution:
             6: [(-1, 0), (0, 1)]
         }
     def dfs(self, grid, i, j, visited):
-        if i == self.m - 1 and j == self.n - 1:
+        if i == self.m -1 and j == self.n - 1:
             return True
         visited[i][j] = True
+
         for dx, dy in self.directions[grid[i][j]]:
-            new_x = i + dx
-            new_y = j + dy
+            new_x , new_y = i + dx, j + dy
 
-            if new_x < 0 or new_x >= self.m or new_y < 0 or new_y >= self.n or visited[new_x][new_y]:
+            if new_x < 0 or new_x >= self.m or new_y < 0 or new_y >= self.n:
                 continue
-
+            if visited[new_x][new_y]:
+                continue
             for bx, by in self.directions[grid[new_x][new_y]]:
                 if new_x + bx == i and new_y + by == j:
                     if self.dfs(grid, new_x, new_y, visited):
                         return True
 
-        return False            
+
+        return False
+
     def hasValidPath(self, grid: List[List[int]]) -> bool:
         self.m = len(grid)
         self.n = len(grid[0])
