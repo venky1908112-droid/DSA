61939fea8636d7b084886589185427a2405a3137
Venkat <venky1908112@gmail.com>
2026-05-02 12:13:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (7.33%) - LeetSync

--- 804-rotated-digits/rotated-digits.py
+++ 804-rotated-digits/rotated-digits.py
@@ -1,25 +1,29 @@
 class Solution:
-    def rotatedDigits(self, n: int) -> int:
-        dp = [0] * (n+1)
-        good = 0
-        for i in range(n + 1):
-            if i < 10:
-                if i in (0, 1, 8):
-                    dp[i] = 1
-                elif i in (2, 5, 6, 9):
-                    dp[i] = 2
-                    good += 1
-                else:
-                    continue
-            else:
-                left = dp[i // 10]
-                last_digit = dp[i % 10]
-                if left == 0 or last_digit == 0:
-                    dp[i] = 0
-                elif left == 1 and last_digit == 1:
-                    dp[i] = 1
-                else:
-                    dp[i] = 2
-                    good += 1
+    m = 10000 
+    dp = [0] * (m+1)
+    good = 0
+    
+    for i in range(1, m + 1):
+        dp[i] = dp[i-1]
+        x = i
+        valid = True
+        change = False
 
-        return good
+        while x > 0:
+            d = x % 10
+
+            if d in (3, 4, 7):
+                valid = False
+                break
+            
+            if d in (2, 5, 6, 9):
+                change = True
+            
+            x //= 10
+
+        if valid and change:
+                dp[i] += 1
+        
+
+    def rotatedDigits(self, n: int) -> int:
+        return self.dp[n]
