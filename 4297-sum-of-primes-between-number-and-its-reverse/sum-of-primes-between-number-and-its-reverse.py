a4e13f1ed3bc4454e580d9504938ba75f26e189b
Venkat <venky1908112@gmail.com>
2026-05-03 11:05:46 +0530

    Time: 5 ms (97.62%) | Memory: 19.1 MB (96.90%) - LeetSync

--- 4297-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.py
+++ 4297-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.py
@@ -1,14 +1,18 @@
 class Solution:
     is_primes = [True] * 1001
-    is_primes[0] = is_primes[1] = False
     p = 2
+    is_primes[0] = is_primes[1] = False
     while p * p <= 1000:
         if is_primes[p]:
-            for multiple in range(p*p, 1001, p):
+            for multiple in range(p * p, 1001, p):
                 is_primes[multiple] = False
         p += 1
     def sumOfPrimesInRange(self, n: int) -> int:
         rev = int(str(n)[::-1])
-        mn = min(n, rev)
-        mx = max(n, rev)
-        return sum(i for i in range(mn, mx + 1) if self.is_primes[i])
+        mn = min(rev, n)
+        mx = max(rev, n)
+        s = 0
+        for i in range(mn, mx + 1):
+            if self.is_primes[i]:
+                s += i
+        return s
