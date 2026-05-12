6e7a28e552ef4c53e1f97a48431970e7b1cce4ff
Venkat <venky1908112@gmail.com>
2026-05-12 16:10:39 +0530

    Delete 1784-minimum-initial-energy-to-finish-tasks directory

--- 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def minimumEffort(self, tasks: List[List[int]]) -> int:
-        tasks.sort(key = lambda x : x[1] - x[0])
-        energy = 0
-        for a,m in tasks:
-            energy = max(energy + a, m)
-        return energy
