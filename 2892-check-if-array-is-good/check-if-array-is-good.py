daf016cde482e754759fd2f20a71300efcd990e7
Venkat <venky1908112@gmail.com>
2026-05-14 09:55:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (59.43%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.py
+++ 2892-check-if-array-is-good/check-if-array-is-good.py
@@ -1,12 +1,11 @@
 class Solution:
     def isGood(self, nums: List[int]) -> bool:
         nums.sort()
-        n = len(nums)
         c = 1
         for x in nums[:-1]:
             if x != c:
                 return False
             c += 1
-        return nums[-1] == n - 1
+        return nums[-1] == len(nums) - 1
         
 
