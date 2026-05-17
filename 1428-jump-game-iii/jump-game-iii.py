89a17d07978e4f34145a1e9d7bee8e071fceaca4
Venkat <venky1908112@gmail.com>
2026-05-17 10:01:49 +0530

    Time: 16 ms (20.47%) | Memory: 39.8 MB (14.57%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.py
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -1,21 +1,12 @@
-from collections import deque
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
         n = len(arr)
         seen = set()
-        q = deque()
-        q.append((start, arr[start]))
-        seen.add(start)
-        while q:
-            idx, val = q.popleft()
-            if val == 0:
+        def dfs(i):
+            if i < 0 or i >= n or i in seen:
+                return False
+            if arr[i] == 0:
                 return True
-            p1 = idx - val
-            p2 = idx + val
-            if p1 >= 0 and p1 not in seen:
-                q.append((p1, arr[p1]))
-                seen.add(p1)
-            if p2 < n and p2 not in seen:
-                q.append((p2, arr[p2]))
-                seen.add(p2)
-        return False
+            seen.add(i)
+            return dfs(i - arr[i]) or dfs(i + arr[i])
+        return dfs(start)
