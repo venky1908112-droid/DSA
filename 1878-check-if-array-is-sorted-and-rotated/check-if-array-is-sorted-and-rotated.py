3fbd46256f05f72cf20db3a997cf894a9c641d50
Venkat <venky1908112@gmail.com>
2026-05-23 12:11:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (86.93%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -4,17 +4,15 @@ class Solution:
 
         if n == 1:
             return True
-
+        
         i = 0
-
         while i < n - 1 and nums[i] <= nums[i + 1]:
             i += 1
-
-        for j in range(i + 1, i + n):
+        
+        for j in range(i + 1,i + n):
             curr = j % n
             nxt = (j + 1) % n
-
             if nums[curr] > nums[nxt]:
                 return False
-
+        
         return True
