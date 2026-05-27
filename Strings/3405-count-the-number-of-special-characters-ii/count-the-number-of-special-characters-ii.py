4c8813b47d64efaf6f8738f3763c691b90624963
Venkat <venky1908112@gmail.com>
2026-05-27 10:19:22 +0530

    leetcode 3121

--- Strings/3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
+++ Strings/3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
@@ -1,21 +1,18 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        special = 0
         lowercase = {}
-        uppercase = {}
-        visited = set()
         for i, letter in enumerate(word):
             if 'a' <= letter <= 'z':
                 lowercase[letter] = i
-            else:
-                if letter not in visited:
-                    uppercase[letter] = i
-                    visited.add(letter)
-        for character, index in uppercase.items():
-            lwr = chr(ord(character) + 32)
-            if lwr not in lowercase:
+        uppercase = {}
+        for i in range(len(word) - 1,  - 1, - 1):
+            if 'A' <= word[i] <= 'Z':
+                uppercase[word[i]] = i
+        special = 0
+        for letter, index in uppercase.items():
+            if letter.lower() not in lowercase:
                 continue
-            if lowercase[lwr] < index:
+            if lowercase[letter.lower()] < index:
                 special += 1
         return special
-
+        
