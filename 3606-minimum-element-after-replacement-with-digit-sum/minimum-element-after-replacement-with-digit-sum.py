6f642924310160341edccc2d1e8cf7c44c15f6ba
Venkat <venky1908112@gmail.com>
2026-05-29 10:16:37 +0530

    Time: 4 ms (51.47%) | Memory: 19.3 MB (56.38%) - LeetSync

---
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+        mn = float('inf')
+        for num in nums:
+            s = 0 
+            for digit in str(num):
+                s += int(digit)
+            mn = min(mn, s)
+        return mn
