dd5bd648ead54ce1d660770dc4662f58a597f27b
Venkat <venky1908112@gmail.com>
2026-06-02 11:39:59 +0530

    Time: 8 ms (79.07%) | Memory: 19.2 MB (98.26%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.py
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.py
@@ -1,11 +1,11 @@
 class Solution:
     def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
         def solve(ls, ld, ws, wd):
-            m = float('inf')
+            finish = float('inf')
             for i in range(len(ls)):
-                m = min(m, ls[i] + ld[i])
+                finish = min(finish, ls[i] + ld[i])
             ans = float('inf')
             for i in range(len(ws)):
-                ans = min(ans, max(m, ws[i]) + wd[i])
+                ans = min(ans, max(finish, ws[i]) + wd[i])
             return ans
-        return min(solve(landStartTime, landDuration, waterStartTime, waterDuration), solve( waterStartTime, waterDuration, landStartTime, landDuration))
+        return min(solve(landStartTime, landDuration, waterStartTime, waterDuration), solve(waterStartTime, waterDuration, landStartTime, landDuration))
