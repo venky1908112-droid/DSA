68a6b16e91cc76bf8d38da1d974e4786998b6231
Venkat <venky1908112@gmail.com>
2026-06-11 11:24:56 +0530

    Time: 425 ms (35.48%) | Memory: 62 MB (95.70%) - LeetSync

--- 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.py
+++ 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.py
@@ -2,19 +2,20 @@ from collections import deque
 class Solution:
     def assignEdgeWeights(self, edges: List[List[int]]) -> int:
         n = len(edges) + 1
-        adj = [[] for i in range(n + 1)]
+        adj = [[] for _ in range(n + 1)]
         for u, v in edges:
             adj[u].append(v)
             adj[v].append(u)
-        """1 : [2, 3]
+        """
+        1 : [2, 3]
         2 : [1]
         3 : [4, 5]
         4 : [3]
         5 : [3]
-"""
+        """
+        mx_depth = 0
         q = deque()
         q.append((1, 0))
-        mx_depth = 0
         visited = [False] * (n + 1)
         while q:
             node, depth = q.popleft()
@@ -23,5 +24,5 @@ class Solution:
             for child in adj[node]:
                 if not visited[child]:
                     q.append((child, depth + 1))
-                    visited[child] = True
         return pow(2, mx_depth - 1, 10 ** 9 + 7)
+        
