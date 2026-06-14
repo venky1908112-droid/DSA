9a3b8332e3dc410150bdf2ef5e6d0d808c21ed4f
Venkat <venky1908112@gmail.com>
2026-06-14 09:06:30 +0530

    Time: 81 ms (21.28%) | Memory: 62.5 MB (34.07%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
@@ -3,15 +3,20 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-from collections import deque
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        q = deque()
+        stack = []
+        l = 0
         temp = head
         while temp:
-            q.append(temp)
             temp = temp.next
+            l += 1
+        temp = head
         mx = 0
-        while q:
-            mx = max(q.popleft().val + q.pop().val, mx)
+        for i in range(l):
+            if i < (l // 2):
+                stack.append(temp.val)
+            else:
+                mx = max(stack.pop() + temp.val, mx)
+            temp = temp.next
         return mx
