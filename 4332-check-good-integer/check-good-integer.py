171d57d9ea5f12e3d2d42ee886c7a33725535d9d
Venkat <venky1908112@gmail.com>
2026-06-15 08:33:57 +0530

    Delete 4332-check-good-integer directory

--- 4332-check-good-integer/check-good-integer.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def checkGoodInteger(self, n: int) -> bool:
-        sq = ds = 0
-        while n > 0:
-            m = n % 10
-            sq += m * m
-            ds += m
-            n //= 10
-        return (sq - ds) >= 50
