b67104e8717de3153a17764e9d37957b541732a0
Venkat <venky1908112@gmail.com>
2026-06-17 15:23:42 +0530

    Time: 368 ms (43.75%) | Memory: 25.1 MB (37.50%) - LeetSync

--- 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.py
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.py
@@ -1,53 +1,29 @@
 class Solution:
     def processStr(self, s: str, k: int) -> str:
-        lengths = [0]
-
-        # Store length after each operation
-        for ch in s:
-            curr = lengths[-1]
-
-            if ch == '*':
-                lengths.append(max(0, curr - 1))
-            elif ch == '#':
-                lengths.append(curr * 2)
-            elif ch == '%':
-                lengths.append(curr)
+        length = [0]
+        for x in s:
+            curr = length[-1]
+            if x == '*':
+                length.append(curr - 1 if curr > 0 else 0)
+            elif x == '#':
+                length.append(curr * 2)
+            elif x == '%':
+                length.append(curr)
             else:
-                lengths.append(curr + 1)
-
-        # k is 0-indexed
-        if k >= lengths[-1]:
+                length.append(curr + 1)
+        if k >= length[-1]:
             return '.'
-
         target = k
-
-        # Trace the target index backwards
         for i in range(len(s) - 1, -1, -1):
             ch = s[i]
-            before = lengths[i]
-
-            if ch == '#':
-                # before: ABC
-                # after : ABCABC
-                if target >= before:
-                    target -= before
-
-            elif ch == '%':
-                # before: ABCDE
-                # after : EDCBA
-                target = before - 1 - target
-
-            elif ch == '*':
-                # before: ABCD
-                # after : ABC
-                # surviving indices stay the same
+            if ch == '*':
                 pass
-
+            elif ch == '#':
+                if target >= length[i]:
+                    target -= length[i]
+            elif ch == '%':
+                target = length[i] - 1 - target
             else:
-                # before: ABC
-                # after : ABCx
-                # x is added at index 'before'
-                if target == before:
+                if target == length[i]:
                     return ch
-
         return '.'
