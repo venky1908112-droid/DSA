ca38cd044148a363bd8fd5591c6de351cb821a86
Venkat <venky1908112@gmail.com>
2026-06-22 05:56:56 +0530

    Time: 3 ms (61.87%) | Memory: 19.3 MB (38.08%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,10 +1,8 @@
-
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        check = "balon"
         d = {'b' : 0, 'a' : 0, 'l' : 0, 'o' : 0, 'n' : 0}
         for x in text:
-            if x in check:
+            if x in 'balon':
                 d[x] += 1
         ans = float('inf')
         for key, val in d.items():
