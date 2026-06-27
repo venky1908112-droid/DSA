06f669302de820c7498ec178c6e96d5f1b457e1c
Venkat <venky1908112@gmail.com>
2026-06-27 17:53:20 +0530

    Time: 102 ms (78.74%) | Memory: 31.6 MB (66.93%) - LeetSync

---
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.py
@@ -0,0 +1,19 @@
+from collections import Counter
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        freq = Counter(nums)
+        one_count = freq[1]
+        if one_count > 0:
+            if not one_count & 1:
+                one_count -= 1
+        ans = one_count
+        del freq[1]
+        curr_ans = 0
+        for key, val in freq.items():
+            curr_ans = 0
+            x = key
+            while x in freq and freq[x] > 1:
+                x = x * x
+                curr_ans += 2
+            ans = max(ans, curr_ans + (1 if x in freq else -1))
+        return ans
