6c0296902df61f0b08e50c50fd8f2632ae21b380
Venkat <venky1908112@gmail.com>
2026-07-06 06:45:22 +0530

    Time: 7 ms (27.07%) | Memory: 19.4 MB (96.95%) - LeetSync

---
+++ 1222-remove-covered-intervals/remove-covered-intervals.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        n = len(intervals)
+        removed = [False] * n
+        for i in range(n):
+            if removed[i]:
+                continue
+            for j in range(i + 1,n):
+                if removed[j] or removed[i]:
+                    continue
+                if intervals[i][0] <= intervals[j][0] and intervals[j][1] <= intervals[i][1]:
+                    removed[j] = True
+                elif intervals[j][0] <= intervals[i][0] and intervals[i][1] <= intervals[j][1]:
+                    removed[i] = True
+        return n - sum(removed)
