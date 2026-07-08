7bc0a264a39a1b842d22d08b42fec363cb8cfa68
Venkat <venky1908112@gmail.com>
2026-07-08 21:36:20 +0530

    Time: 293 ms (77.51%) | Memory: 83.5 MB (62.23%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.py
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.py
@@ -2,19 +2,19 @@ class Solution:
     def containsCycle(self, grid: List[List[str]]) -> bool:
         m = len(grid)
         n = len(grid[0])
-        visited= [[False] * n for _ in range(m)]
         directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        visited = [[False] * n for _ in range(m)]
         def dfs(x, y, px, py):
             visited[x][y] = True
             for dx, dy in directions:
                 nx, ny = x + dx, y + dy
 
-                if not(0 <= nx < m and 0 <= ny < n):
+                if not (0 <= nx < m and 0 <= ny < n):
                     continue
 
-                if grid[x][y] != grid[nx][ny]:
+                if grid[nx][ny] != grid[x][y]:
                     continue
-                
+
                 if nx == px and ny == py:
                     continue
                 
@@ -23,14 +23,14 @@ class Solution:
                 
                 if dfs(nx, ny, x, y):
                     return True
-                
+            
             return False
-
-
 
         for i in range(m):
             for j in range(n):
-                if not visited[i][j]:
-                    if dfs(i, j, -1, -1):
-                        return True
+                if visited[i][j]:
+                    continue
+                if dfs(i, j, -1, -1):
+                    return True
         return False
+            
