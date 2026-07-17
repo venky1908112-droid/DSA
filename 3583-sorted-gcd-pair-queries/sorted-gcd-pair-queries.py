f509aae0f2ab2e287a17bb7857708e1610b82195
Venkat <venky1908112@gmail.com>
2026-07-17 21:54:20 +0530

    Time: 245 ms (100.00%) | Memory: 39.2 MB (82.50%) - LeetSync

--- 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.py
+++ 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.py
@@ -8,13 +8,12 @@ class Solution:
         gcd = [0] * (mx + 1)
         for i in range(mx, 0, -1):
             sm = sum(freq[i::i])
-            t_pairs = (sm * (sm - 1)) // 2
-            gcd[i] = t_pairs - sum(gcd[i::i])
+            t_p = (sm * (sm - 1)) // 2
+            valid = t_p - sum(gcd[i::i])
+            gcd[i] = valid
         prefix = list(accumulate(gcd))
         res = []
         for q in queries:
             res.append(bisect.bisect_right(prefix, q))
         return res
-            
-
         
