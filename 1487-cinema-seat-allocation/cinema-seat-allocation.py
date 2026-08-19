f9916afe9a4751f6074a729fe06ffd688b9547a3
Venkat <venky1908112@gmail.com>
2026-08-19 18:22:25 +0530

    Time: 81 ms (28.28%) | Memory: 22.5 MB (89.46%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.py
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.py
@@ -1,27 +1,25 @@
 class Solution:
     def maxNumberOfFamilies(self, total_rows: int, a: List[List[int]]) -> int:
         a.sort()
-        present_rows = 0
-        ans = 0
-        n = len(a)
         i = 0
+        n = len(a)
+        ans = 0
+        present_rows = 0
         while i < n:
             present_rows += 1
             x, y = a[i]
-            r = 0
-            while i < n and x == a[i][0]:
-                r = r | (1 << (10 - a[i][1]))
+            bit = 0
+            while i < n and a[i][0] == x:
+                bit |= (1 << (10 - a[i][1]))
                 i += 1
-            r >>= 1
+            bit >>= 1
             end = 9
-            while end >= 4:
-                if r & 15 == 0:
+            while end > 4:
+                if bit & 15 == 0:
                     ans += 1
-                    r >>= 4
+                    bit >>= 4
                     end -= 4
                     continue
-                
-                r >>= 2
+                bit >>= 2
                 end -= 2
-                
-        return ans + (total_rows - present_rows) * 2 
+        return ans + (total_rows - present_rows) * 2
