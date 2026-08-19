0e93b3ce3065837ec3787833781f9c1de269f55c
Venkat <venky1908112@gmail.com>
2026-08-19 11:19:47 +0530

    Time: 7 ms (57.23%) | Memory: 21.3 MB (18.50%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,7 @@
+from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = 0
-        ans = -1
-        for x in nums:
-            if count == 0:
-                ans = x
-                count = 1
-            elif x == ans:
-                count += 1
-            else:
-                count -= 1
-        return ans
+        freq = Counter(nums)
+        for key, val in freq.items():
+            if val > len(nums) // 2:
+                return key
