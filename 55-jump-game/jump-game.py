2fed84a8cc5a4d753ccd3f63757dec4420b29286
Venkat <venky1908112@gmail.com>
2026-09-13 17:50:04 +0530

    updated

--- 55-jump-game/jump-game.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        reached = 0
-        for i in range(len(nums)):
-            if reached < i:
-                return False
-            reached = max(i + nums[i], reached)
-        return True
