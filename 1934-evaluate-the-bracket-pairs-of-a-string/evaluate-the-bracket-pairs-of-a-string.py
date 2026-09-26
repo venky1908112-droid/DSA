4b4c06357b9860a98a7839678529e45c4edccdd3
Venkat <venky1908112@gmail.com>
2026-09-26 15:37:10 +0530

    Time: 47 ms (69.16%) | Memory: 51.4 MB (61.68%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+class Solution:
+    def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
+        map = defaultdict(str)
+        for key, val in knowledge:
+            map[key] = val
+        res = ""
+        inside = False
+        for ch in s:
+            if ch == '(':
+                inside = True
+                temp = ""
+            elif ch == ')':
+                inside = False
+                if temp in map:
+                    res += map[temp]
+                else:
+                    res += '?'
+            elif inside:
+                temp += ch
+            else:
+                res += ch
+        return res
